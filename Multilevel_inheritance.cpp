e48e73347e1af4b5c487672f5d3eacf07432ccf1
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-10-09 23:58:35 +0530

    Create Multilevel_inheritance.cpp

---
+++ Multilevel_inheritance.cpp
@@ -0,0 +1,43 @@
+//name: Kshitij Yadav
+//PRN: 23070123075
+//Multilevel inheritance
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Parent Class-1
+class Food 
+{
+    public:
+    string cuisine="Indian";
+    void type()
+    {
+        cout<<"Asian"<<endl;
+    }
+};
+//Child Class-1 (derived from parent-1)
+class Dish: public Food 
+{
+    public:
+    string dish="Biryani";
+};
+//Child Class-2 (derived from child-1)
+class Restaurant: public Dish 
+{
+    public:
+    string name="Spice Kitchen";
+};
+
+int main()
+{
+    //Multilevel Inheritance
+    Restaurant f3;
+    f3.type();
+    cout<<f3.cuisine<<": "<<f3.dish<<endl;
+    cout<<"Restaurant: "<<f3.name;
+}
+/*
+Asian
+Indian: Biryani
+Restaurant: Spice Kitchen
+*/
